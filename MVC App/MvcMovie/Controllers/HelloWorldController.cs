f7ce3d3eea8c321ea4a35ee15ad3049bd7cef85d
WhiteNoise01 <153906600+SEFWHITE-Dev@users.noreply.github.com>
2025-03-06 17:17:39 +0900

    MVC app

---
+++ MVC App/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        // Every public method in a controller is callable as an HTTP endpoint
+        // controller methods are 'action methods', and generally return something derived from ActionResult
+
+        // GET: /HelloWorld/
+        // returns a view result obj
+        // uses a view template to generate HTML response
+        public IActionResult Index()
+        {
+            //return "default action";
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        // optional param defaults to 1 if no value is passed in
+        // uses HtmlEncoder.Default.Encode to protect the app from malicious input e.g. JS
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is : {numTimes}");
+        //}
+
+        // the ViewData dictionary is a dynamic obj
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
---
+++ MVC App/MvcMovie/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } // pk for db
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; } // string? means the property is nullable
+
+        [Display(Name = "Release Date")] // specifies what to display for the name of the field
+        [DataType(DataType.Date)] // specifies the ReleaseDate var to be of a certain type. only date is displayed (not time). no user input is required.
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")] // limit what characters can be used for input
+        [Required] // user must input something (but can still input white space)
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
+
+    }
+}
